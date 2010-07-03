e4246f5c36a3327f2e7488712a390c99408e1d86
Thorbjørn Hermansen <thhermansen@gmail.com>
2010-07-03 16:54:41 +0200

    Did a bit of refactoring. Hopefully have not broken that much ;-)

--- lib/is_paranoid.rb
+++ lib/is_paranoid.rb
@@ -20,45 +20,60 @@ module IsParanoid
         # self.count_with_destroyed, and self.find_with_destroyed )
         default_scope where(:deleted_at => nil)
 
-        # Actually delete the model, bypassing the safety net.  Because
-        # this method is called internally by Model.delete(id) and on the
-        # delete method in each instance, we don't need to specify those
-        # methods separately
-        def self.delete_all conditions = nil
-          self.with_destroyed_scope { super conditions }
+
+
+
+        class << self
+          def destroyed
+            unscoped.where('deleted_at IS NOT NULL')
+          end
+
+          # Actually delete the model, bypassing the safety net.  Because
+          # this method is called internally by Model.delete(id) and on the
+          # delete method in each instance, we don't need to specify those
+          # methods separately
+          def delete_all conditions = nil
+            with_exclusive_scope { super conditions }
+          end
+
+          # Return a count that includes the soft-deleted models.
+          def count_with_destroyed
+            unscoped.count
+          end
+
+          # Perform a count only on destroyed instances.
+          def count_only_destroyed
+            destroyed.count
+          end
+
+          # Return instances of all models matching the query regardless
+          # of whether or not they have been soft-deleted.
+          def find_with_destroyed *args
+            unscoped.find(*args)
+          end
+          
+          # Perform a find only on destroyed instances.
+          def find_only_destroyed *args
+            destroyed.find(*args)
+          end
+
+          # Returns true if the requested record exists, even if it has
+          # been soft-deleted.
+          def exists_with_destroyed? *args
+            unscoped.exists?(*args)
+          end
+
+          # Returns true if the requested record has been soft-deleted.
+          def exists_only_destroyed? *args
+            destroyed.exists?(*args)
+          end
         end
 
-        # Return a count that includes the soft-deleted models.
-        def self.count_with_destroyed *args
-          self.with_destroyed_scope { count(*args) }
-        end
 
-        # Perform a count only on destroyed instances.
-        def self.count_only_destroyed *args
-          self.with_only_destroyed_scope { count(*args) }
-        end
 
-        # Return instances of all models matching the query regardless
-        # of whether or not they have been soft-deleted.
-        def self.find_with_destroyed *args
-          self.with_destroyed_scope { find(*args) }
-        end
-        
-        # Perform a find only on destroyed instances.
-        def self.find_only_destroyed *args
-          self.with_only_destroyed_scope { find(*args) }
-        end
 
-        # Returns true if the requested record exists, even if it has
-        # been soft-deleted.
-        def self.exists_with_destroyed? *args
-          self.with_destroyed_scope { exists?(*args) }
-        end
 
-        # Returns true if the requested record has been soft-deleted.
-        def self.exists_only_destroyed? *args
-          self.with_only_destroyed_scope { exists?(*args) }
-        end
+
 
         # Override the default destroy to allow us to flag deleted_at.
         # This preserves the before_destroy and after_destroy callbacks.
@@ -87,49 +102,11 @@ module IsParanoid
         
         protected
 
-        # Mark the model deleted_at as now.
-        def destroy_without_callbacks
-          self.deleted_at = current_time_from_proper_timezone
-          update_without_callbacks
-        end
-        
-        def self.with_only_destroyed_scope(&block)
-          with_destroyed_scope do
-            table = connection.quote_table_name(table_name)
-            attr = connection.quote_column_name(:deleted_at)
-            with_scope(:find => { :conditions => "#{table}.#{attr} IS NOT NULL" }, &block)
+          # Mark the model deleted_at as now.
+          def destroy_without_callbacks
+            self.deleted_at = current_time_from_proper_timezone
+            update_without_callbacks
           end
-        end
-
-        def self.with_destroyed_scope
-          scope = current_scoped_methods
-
-          if scope.include?(:find) and scope[:find].include?(:conditions)
-            scope = scope.dup
-            scope[:find] = scope[:find].dup
-            conditions = scope[:find][:conditions] = scope[:find][:conditions].dup
-
-            case conditions
-            when Hash then
-              if conditions[:deleted_at].nil?
-                conditions.delete(:deleted_at)
-              end
-            when String then
-              c = sanitize_conditions(:deleted_at => nil)
-              conditions.gsub!(c, '1=1')
-            end
-
-            self.scoped_methods << scope
-
-            begin
-              yield
-            ensure
-              self.scoped_methods.pop
-            end
-          else
-            yield
-          end
-        end
       end
     end
   end
